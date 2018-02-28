4874bdc4fba357dcdfc5906fa0fbe54c0866e0a9
krishnapraneeth.p <krishnapraneeth.p@KrishnaPraneeth-Thinkpad>
2018-02-28 22:13:54 +0530

    Spring Boot Quick Start - Starting a Spring Boot application

---
+++ course-api/src/main/java/tuts/javabrains/springbootstarter/CourseApiInitializer.java
@@ -0,0 +1,13 @@
+package tuts.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiInitializer.class, args);
+	}
+
+}
