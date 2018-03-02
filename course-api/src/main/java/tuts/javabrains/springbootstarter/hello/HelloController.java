d1b09dc10a419103ac697ea804b3e55ed9e1d506
krishnapraneeth.p <krishnapraneeth.p@KrishnaPraneeth-Thinkpad>
2018-03-02 10:37:51 +0530

    Spring Boot Quick Start - Adding a REST Controller

---
+++ course-api/src/main/java/tuts/javabrains/springbootstarter/hello/HelloController.java
@@ -0,0 +1,13 @@
+package tuts.javabrains.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi";
+	}
+}
